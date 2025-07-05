10cd8627e289c4385ee3d02a2fae4f31669feddb
Rudzeus <croyale760@gmail.com>
2025-07-05 16:25:44 +0700

    Add files via upload

---
+++ resources/views/includes/landing/navbar.blade.php
@@ -0,0 +1,23 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1> <a><img src="assets/img/OIP.jpeg" class="img-fluid" width="50" height="50"><a>  PENGADUAN KAMITUWO</a></h1>
+        <!-- href="index.html" href="#"-->
+        <!-- Uncomment below if you prefer to use an image logo -->
+       
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Tentang kami</a></li>   
+          <div> <a class="border-solid" href="{{ url('login')}}">Masuk</a></div>
+          <div> <a class="border-solid" href="{{ url('register')}}">Daftar</a></div>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
